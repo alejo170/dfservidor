63a7574ea4977db786b8f872714883d0cc0e7038
Alejandro <alejo170@gmail.com>
2021-10-24 14:41:00 -0500

    Probando

---
+++ config/config.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+module.exports = {
+  'development': {
+    username: process.env.DB_USERNAME || "sql10446385",
+    password: process.env.DB_PASSWORD || "ceBKwdhQRW",
+    database: process.env.DB_DATABASE || "sql10446385",
+    host: process.env.DB_HOST || "sql10.freesqldatabase.com",
+    dialect: process.env.DB_DIALECT || "mysql",
+  }
+  
+}
